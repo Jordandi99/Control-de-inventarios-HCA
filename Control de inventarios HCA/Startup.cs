e27330b8a2e8e26f368bfaf8fcb46f8debc8cce5
Jordandi99 <reclevel2k@gmail.com>
2021-10-06 12:20:38 -0500

    Agregar archivos de proyecto.

---
+++ Control de inventarios HCA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Control_de_inventarios_HCA.Startup))]
+namespace Control_de_inventarios_HCA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
